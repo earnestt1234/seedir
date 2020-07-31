2cb21a2396bf31dea5d33bdbac461eaf97ddc13b
Tom Earnest <31780412+earnestt1234@users.noreply.github.com>
2020-07-31 18:51:02 -0500

    adding indent

--- src/seedir.py
+++ src/seedir.py
@@ -7,16 +7,22 @@ Created on Fri Jul 24 15:34:12 2020
 
 import os
 
-STYLE_DICT = {'lines':{'split':'├───',
-                       'dline':'│   ',
-                       'space':'    ',
-                       'final':'└───'},
-              'spaces':{'split':'    ',
-                        'dline':'    ',
-                        'space':'    ',
-                        'final':'    '}}
+import emoji
 
-def recursive_print(path, level=0, incomplete=[], split='├───', dline='│   ', space='    ', final='└───'):
+STYLE_DICT = {'lines':{'split':'├─',
+                       'dline':'│ ',
+                       'space':'  ',
+                       'final':'└─'},
+              'spaces':{'split':'  ',
+                        'dline':'  ',
+                        'space':'  ',
+                        'final':'  '}}
+
+class SeedirError(Exception):
+    """Class for representing errors from seedir"""
+
+def recursive_print(path, level=0, incomplete=[], split='├─', dline='│ ',
+                    space='  ', final='└─'):
     output = ''
     last = False
     if level == 0:
@@ -47,12 +53,29 @@ def recursive_print(path, level=0, incomplete=[], split='├───', dline='�
 
 def get_style(style):
     if style not in STYLE_DICT:
-        raise Exception
+        error_text = 'style "{}" not recognized, must be '.format(style)
+        error_text += 'lines, spaces, or emoji'
+        raise SeedirError(error_text)
     else:
         return STYLE_DICT[style]
 
-def seedir(path, style='lines'):
+def format_style(style_dict, indent=2):
+    if indent < 0 or not isinstance(indent, int):
+        raise SeedirError('indent must be a positive integer')
+    elif indent == 0:
+        output = {k : '' for k,_ in style_dict.items()}
+    elif indent == 1:
+        output = {k : v[0] for k,v in style_dict.items()}
+    elif indent == 2:
+        output = style_dict
+    else:
+        indent -= 2
+        output = {k : v + v[-1]*indent for k,v in style_dict.items()}
+    return output
+
+def seedir(path, style='lines', indent=2):
     styleargs = {}
     if style:
         styleargs = get_style(style)
+    styleargs = format_style(styleargs, indent=indent)
     return recursive_print(path, **styleargs)
