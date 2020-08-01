32f2aaee5ca21944d672290432e6f591264795f6
Tom Earnest <31780412+earnestt1234@users.noreply.github.com>
2020-08-01 16:29:51 -0500

    adding more styles

--- src/seedir.py
+++ src/seedir.py
@@ -16,17 +16,21 @@ STYLE_DICT = {'lines':{'split':'├─',
               'spaces':{'split':'  ',
                         'dline':'  ',
                         'space':'  ',
-                        'final':'  '}}
+                        'final':'  '},
+              'plus':{'split':'+-',
+                       'dline':'| ',
+                       'space':'  ',
+                       'final':'+-'}}
 
 class SeedirError(Exception):
     """Class for representing errors from seedir"""
 
 def recursive_print(path, level=0, incomplete=[], split='├─', dline='│ ',
-                    space='  ', final='└─'):
+                    space='  ', final='└─', filestart='', folderstart=''):
     output = ''
     last = False
     if level == 0:
-        output += os.path.basename(path) + '/\n'
+        output += folderstart + os.path.basename(path) + os.sep +'\n'
     level += 1
     incomplete.append(level-1)
     for i, f in enumerate(os.listdir(path)):
@@ -43,21 +47,37 @@ def recursive_print(path, level=0, incomplete=[], split='├─', dline='│ ',
         if last:
             incomplete.remove(level-1)
         if os.path.isdir(sub):
-            output += header + f + '/\n'
+            output += header + folderstart + f + os.sep +'\n'
             output += recursive_print(sub, level=level, incomplete=incomplete,
                                       split=split, dline=dline, space=space,
-                                      final=final)
+                                      final=final, filestart=filestart,
+                                      folderstart=folderstart)
         else:
-            output += header + f + '\n'
+            output += header + filestart + f + '\n'
     return output
 
 def get_style(style):
+    if style == 'arrow':
+        style = 'spaces'
+    if style == 'emoji':
+        style = 'lines'
     if style not in STYLE_DICT:
         error_text = 'style "{}" not recognized, must be '.format(style)
-        error_text += 'lines, spaces, or emoji'
+        error_text += 'lines, spaces, arrow, plus, or emoji'
         raise SeedirError(error_text)
     else:
         return STYLE_DICT[style]
+
+def get_file_folder_start(style):
+    d = {'filestart' : '',
+         'folderstart' : ''}
+    if style == 'arrow':
+        d['filestart'] = '>'
+        d['folderstart'] = '>'
+    if style == 'emoji':
+        d['filestart'] = emoji.emojize(':page_facing_up:')
+        d['folderstart'] = emoji.emojize(':file_folder:')
+    return d
 
 def format_style(style_dict, indent=2):
     if indent < 0 or not isinstance(indent, int):
@@ -73,9 +93,18 @@ def format_style(style_dict, indent=2):
         output = {k : v + v[-1]*indent for k,v in style_dict.items()}
     return output
 
-def seedir(path, style='lines', indent=2):
+def seedir(path, style='lines', indent=2, uniform='', **kwargs):
     styleargs = {}
+    startargs = {}
     if style:
         styleargs = get_style(style)
+        startargs = get_file_folder_start(style)
     styleargs = format_style(styleargs, indent=indent)
-    return recursive_print(path, **styleargs)
+    allargs = {**styleargs, **startargs}
+    for k in kwargs:
+        if k in allargs:
+            allargs[k] = kwargs[k]
+    if uniform:
+        for arg in ['dline', 'split', 'final', 'space']:
+            allargs[arg] = uniform
+    return recursive_print(path, **allargs)
