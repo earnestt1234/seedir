d53de171dc0a516da33d21a909a88a8e8ff9e8db
Tom Earnest <31780412+earnestt1234@users.noreply.github.com>
2020-09-07 15:14:02 -0500

    Update tests.py
    adding more unittests

--- tests/tests.py
+++ tests/tests.py
@@ -19,6 +19,7 @@ With the command:
 
 Test methods MUST start with "test"
 """
+import os
 import unittest
 
 import seedir as sd
@@ -34,12 +35,48 @@ example = """mypkg/
 
 example_with_comments="""mypkg/
     __init__.py #some comments
-    app.py #more comments
-    view.py #     how about## this one
+    app.py #####        more comments
+    view.py #how about## this one
     test/
         __init__.py
         test_app.py
         test_view.py"""
+
+try:
+    testdir = os.path.join(os.environ['USERPROFILE'], 'Desktop')
+except:
+    try:
+        testdir = os.environ['USERPROFILE']
+    except:
+        try:
+            testdir = os.path.dirname(os.path.abspath(__file__))
+        except:
+            testdir = input('Cannot automatically find a directory for '
+                            'testing seedir.seedir() - please enter a '
+                            'path to test with depthlimit=1:\n')
+            while not os.path.isdir(testdir):
+                testdir = input('Not found, try again:\n')
+
+class PrintSomeDirs(unittest.TestCase):
+    print('\n--------------------'
+          '\n\nTesting seedir.seedir() against {}:\n\n'
+          '--------------------'
+          '\n'.format(testdir))
+    def test_a_print_userprofile(self):
+        print('Basic seedir (depthlimit=2):\n')
+        sd.seedir(testdir, depthlimit=2)
+
+    def test_b_styles(self):
+        print('\nDifferent Styles (depthlimit=1, itemlimit=5):')
+        for style in sd.STYLE_DICT.keys():
+            print('\n{}:\n'.format(style))
+            sd.seedir(testdir, style=style, depthlimit=1, itemlimit=5)
+
+    def test_c_indent(self):
+        print('\nDifferent Indents (depthlimit=1, itemlimit=5):')
+        for i in list(range(3)) + [8]:
+            print('\nindent={}:\n'.format(str(i)))
+            sd.seedir(testdir, depthlimit=1, itemlimit=5, indent=i)
 
 class TestFakeDirFromString(unittest.TestCase):
     def test_read_string(self):
