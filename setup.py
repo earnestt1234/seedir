74e36de743399eeb3b25490f5e4033b7b9514192
Tom Earnest <31780412+earnestt1234@users.noreply.github.com>
2020-09-27 15:26:48 -0500

    updating setup.py for pypl

--- setup.py
+++ setup.py
@@ -6,10 +6,20 @@ Setup for seedir.
 @author: Tom Earnest
 """
 
+from os import path
+
 from setuptools import setup
 
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+long_description = '\n'.join([l for l in long_description.splitlines()
+                              if not l.strip().startswith('<')])
+
 setup(name='seedir',
-      version='0.1',
+      version='0.1.1',
       description='Package for creating, editing, and reading folder tree diagrams.',
       url='https://github.com/earnestt1234/seedir',
       author='Tom Earnest',
@@ -19,7 +29,8 @@ setup(name='seedir',
       install_requires=['emoji', 'natsort'],
       include_package_data=True,
       zip_safe=False,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       entry_points = {
         'console_scripts': ['seedir=seedir.command_line:main'],
         })
-
