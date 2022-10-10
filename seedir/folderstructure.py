c1180ee9dd001c546740c140bd826add1563807b
Tom Earnest <earnestt1234@gmail.com>
2022-10-09 23:13:04 -0500

    work on moving helper functions to methods of FolderStructure

--- seedir/folderstructure.py
+++ seedir/folderstructure.py
@@ -7,6 +7,9 @@ The primary algorithm for determining the folder structure returned by
 
 import os
 
+import natsort
+
+from seedir.errors import SeedirError
 from seedir.folderstructurehelpers import (listdir_fullpath,
                                            sort_dir,
                                            sort_fakedir,
@@ -19,6 +22,7 @@ from seedir.folderstructurehelpers import (listdir_fullpath,
 
 class FolderStructure:
     '''General class for determining folder strctures.'''
+
     def __init__(self, listdir_func, sort_func, filter_func, isdir_func,
                  getname_func, beyondstr_func):
         '''
@@ -56,6 +60,36 @@ class FolderStructure:
         self.isdir = isdir_func
         self.getname = getname_func
         self.beyondstr = beyondstr_func
+
+    def __call__(self, folder, **kwargs):
+        return self._folderstructure(folder, **kwargs).strip()
+
+    def _beyond_depth_str(self, items, beyond):
+
+        if beyond.lower() == 'ellipsis':
+            return '...'
+        elif beyond.lower() in ['contents','content']:
+            folders = self._count_folders(items)
+            files = self._count_files(items)
+            return '{} folder(s), {} file(s)'.format(folders, files)
+        elif beyond and beyond[0] == '_':
+            return beyond[1:]
+        else:
+            s1 = '"beyond" must be "ellipsis", "content", or '
+            s2 = 'a string starting with "_"'
+            raise SeedirError(s1 + s2)
+
+    def _count_files(self, items):
+        files = sum([not self.isdir(i) for i in items])
+        return files
+
+    def _count_folders(self, items):
+        folders = sum([self.isdir(i) for i in items])
+        return folders
+
+    # def _filter_items(listdir, include_folders=None,
+    #                   exclude_folders=None, include_files=None,
+    #                   exclude_files=None, regex=False, mask=None):
 
     def _folderstructure(self, folder, default_args, depth=0, incomplete=None,
                          extend='│ ', space='  ', split='├─', final='└─',
@@ -214,8 +248,64 @@ class FolderStructure:
 
         return output
 
-    def __call__(self, folder, **kwargs):
-        return self._folderstructure(folder, **kwargs).strip()
+    def _formatter_update_args(formatter, item, args):
+        '''
+        Update a dictionary of style tokens based on a formatter function and
+        an item.  Added in v 0.3.0 to support the addition of the formatter parameter
+        for the seedir algorithm.
+
+        Parameters
+        ----------
+        formatter : function
+            Formatting function.
+        item : file or folder item
+            String or FakeItem produced in the seedir algorithm
+        styleargs : dict
+            Dictionary of seedir style tokens
+
+        Returns
+        -------
+        styleargs : dict
+            The input dictionary, after it may have been updated.
+
+        '''
+        newstyle = formatter(item)
+        if newstyle is None:
+            pass
+        else:
+            args.update(newstyle)
+
+        return args
+
+    def _get_base_header(incomplete, extend, space):
+        base_header = []
+        max_i = max(incomplete)
+        for p in range(max_i):
+            if p in incomplete:
+                base_header.append(extend)
+            else:
+                base_header.append(space)
+        return "".join(base_header)
+
+    def _sort_dir(self, items, first=None, sort_reverse=False, sort_key=None):
+
+        if sort_key is None:
+            key = lambda x : self.getname(x)
+        else:
+            key = lambda x: sort_key(self.getname(x))
+
+        if first in ['folders', 'files']:
+            folders = [p for p in items if self.isdir(p)]
+            files = [p for p in items if not self.isdir(p)]
+            folders = natsort.natsorted(folders, reverse=sort_reverse, key=key)
+            files = natsort.natsorted(files, reverse=sort_reverse, key=key)
+            output = folders + files if first == 'folders' else files + folders
+        elif first is None:
+            output = list(natsort.natsorted(items, reverse=sort_reverse, key=key))
+        else:
+            raise ValueError("`first` must be 'folders', 'files', or None.")
+
+        return output
 
 slashes = os.sep + '/' + '//'
 
