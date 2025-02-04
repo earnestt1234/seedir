adf2fde21d81e92d126b9bb82fdba8733e221a3c
Tom Earnest <31780412+earnestt1234@users.noreply.github.com>
2025-02-03 22:00:18 -0600

    add pyproject.toml create __version__.py

--- setup.py
+++ setup.py
@@ -15,8 +15,12 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+# read version
+with open(path.join(this_directory, 'seedir', '__version__.py'), encoding='utf-8') as f:
+    version = f.read().split('=')[1].strip('\'"\n')
+
 setup(name='seedir',
-      version='0.5.0',
+      version=version,
       description='Package for creating, editing, and reading folder tree diagrams.',
       url='https://github.com/earnestt1234/seedir',
       author='Tom Earnest',
